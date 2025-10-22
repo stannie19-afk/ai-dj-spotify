cc4c73387832010b23744375d99d0c8574b12123
Stan Bruinvelds <stannie19@hotmail.com>
2025-10-22 22:19:00 +0200

    Add debug route to check public folder

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const axios = require('axios');
 const path = require('path');
-const fs = require('fs'); 
+const fs = require('fs');  //
 require('dotenv').config();
 
 // App initialiseren
